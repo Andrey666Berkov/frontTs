c45fd2f3e665d8752d70d545a14f196bce1d71b1
andreyberkov666 <andreyberkov666@mail.ru>
2024-12-06 16:19:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Product } from "./components/Product";
+
+function App() {
+	return (
+		<div className=" container mx-auto max-w-2xl pt-5">
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/data/products.ts
@@ -0,0 +1,13 @@
+export const products = [
+	{
+		id: 1,
+		title: "Hello world",
+		price: 109.99,
+		description: "nfkgjdsbngviju;anbreiuo;jgvb;irehjutabnv;hj",
+		category: "https://fakefdasfasd.com",
+		rating: {
+			rate: 3.9,
+			count: 120,
+		},
+	},
+];
